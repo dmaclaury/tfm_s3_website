a9b35cb090a7613f050132a12b6e474024d44ec4
Daniel MacLaury <1966417+dmaclaury@users.noreply.github.com>
2022-07-07 01:16:51 +0000

    Fix bucket name output

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 # Output website bucket name
 output "s3_bucket_name" {
-    value = aws_s3_bucket_website_configuration.website.website_endpoint
+    value = aws_s3_bucket.website.id
 }
 
 # Output S3 bucket website domain
