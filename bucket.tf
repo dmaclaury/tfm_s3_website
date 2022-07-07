3c23214e9574de3bf2ccc103354119e6b6285ba7
Daniel MacLaury <1966417+dmaclaury@users.noreply.github.com>
2022-07-07 01:23:14 +0000

    Remove public ACL block and add bucket policy

--- bucket.tf
+++ bucket.tf
@@ -17,12 +17,25 @@ resource "aws_s3_bucket_acl" "website" {
   acl    = "public-read"
 }
 
-# Blocking public access to the S3 bucket.
-resource "aws_s3_bucket_public_access_block" "website" {
+# Bucket Policy
+resource "aws_s3_bucket_policy" "website" {
   bucket = aws_s3_bucket.website.id
 
-  block_public_acls   = true
-  block_public_policy = true
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource = [
+          aws_s3_bucket.website.arn,
+          "${aws_s3_bucket.website.arn}/*",
+        ]
+      },
+    ]
+  })
 }
 
 # S3 bucket website settings.
