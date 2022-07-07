23bccc0e25f9c33ea33670f3a7d2335508b41fb9
Daniel MacLaury <1966417+dmaclaury@users.noreply.github.com>
2022-07-07 01:37:10 +0000

    terraform fmt fix

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 # Output website bucket name
 output "s3_bucket_name" {
-    value = aws_s3_bucket.website.id
+  value = aws_s3_bucket.website.id
 }
 
 # Output S3 bucket website domain
 output "s3_bucket_website_url" {
-    value = aws_s3_bucket_website_configuration.website.website_endpoint
+  value = aws_s3_bucket_website_configuration.website.website_endpoint
 }
