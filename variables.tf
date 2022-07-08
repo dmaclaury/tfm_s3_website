ea45f3a9a73388b3aa8a35382581bf898864627f
Daniel MacLaury <1966417+dmaclaury@users.noreply.github.com>
2022-07-08 02:42:15 +0000

    add tld var for getting cloudflare zone

--- variables.tf
+++ variables.tf
@@ -8,3 +8,8 @@ variable "domain" {
   type        = string
   description = "Domain for the S3 website."
 }
+
+variable "tld" {
+  type        = string
+  description = "Top level domain for cloudflare zone."
+}
