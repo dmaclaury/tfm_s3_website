0b96ad10d733322919d7c50bea48d0e2fcefeed8
Daniel MacLaury <1966417+dmaclaury@users.noreply.github.com>
2022-07-08 01:56:04 +0000

    Add very basic error page

--- bucket_files.tf
+++ bucket_files.tf
@@ -5,3 +5,11 @@ resource "aws_s3_object" "object" {
   source = "web/index.html"
   etag   = filemd5("web/index.html")
 }
+
+# Upload error.html to the bucket
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.website.id
+  key    = "error.html"
+  source = "web/error.html"
+  etag   = filemd5("web/error.html")
+}
