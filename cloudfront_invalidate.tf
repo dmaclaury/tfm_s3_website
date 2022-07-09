2c65fa49010ae67bb5a02740d1f11e9f9fc0c85d
Daniel MacLaury <1966417+dmaclaury@users.noreply.github.com>
2022-07-09 22:59:03 +0000

    Add cloudfront invalidation to clear cache

---
+++ cloudfront_invalidate.tf
@@ -0,0 +1,9 @@
+# Based on https://faun.pub/lets-do-devops-terraform-hacking-s3-and-cloudfront-dependencies-13c8a2af2f20
+resource "null_resource" "invalidate_cf_cache" {
+  provisioner "local-exec" {
+    command = "aws cloudfront create-invalidation --distribution-id ${aws_cloudfront_distribution.s3_distribution.id} --paths '/*'"
+  }
+  depends_on = [
+    aws_s3_object.index.etag,
+  ]
+}
